54faaf4b7b299b39d9660e215657c361ea779d87
unknown <programmer.m.belal@gmail.com>
2025-11-17 22:47:32 +0600

    Problem 2 is solved

--- index.ts
+++ index.ts
@@ -12,3 +12,16 @@ const formatValue=(value: string|number|boolean)=>{
 // console.log(formatValue('hello'));
 // console.log(formatValue(5));
 // console.log(formatValue(true));
+
+
+//Problem 02
+const getLength=(value: string|Array<any>)=>{
+    if(typeof value ==='string'){
+      return  value.length
+    }else{
+       return value.length
+    }
+}
+
+// console.log(getLength('typescript'));
+// console.log(getLength([10, 20, 30, 40]));
